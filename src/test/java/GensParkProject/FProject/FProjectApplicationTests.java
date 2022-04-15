0850cb95640546b79eb1f1f91159026590d95e96
// <kerdja@outlook.com>
2022-04-15 14:47:26 -0500

    first commit

---
+++ src/test/java/GensParkProject/FProject/FProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package GensParkProject.FProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
